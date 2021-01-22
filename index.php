d52834abf01f6543a11aba22ae1a56234b4fe47d
MarcoB <mrc.bsllt@gmail.com>
2021-01-22 16:02:46 +0100

    completato l'esercizio

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <!-- google font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
     <!-- foglio di stile personale -->
     <link rel="stylesheet" href="css/style.css">
     <title>php ajax dischi</title>
@@ -21,7 +24,18 @@
     <main>
       <div class="wrapper">
         <?php foreach ($database as $discContaier) { ?>
-          <div class="disc-container"></div>
+
+          <div class="disc-container">
+            <img src="img/<?= $discContaier["cover"]; ?>" alt="<?= $discContaier["title"]; ?>">
+            <div class="text">
+              <h3><?= $discContaier["title"]; ?></h3>
+              <a href="#">
+                <h4><?= $discContaier["author"]; ?></h4>
+              </a>
+              <span><?= $discContaier["year_pubblication"]; ?></span>
+            </div>
+          </div>
+
         <?php } ?>
       </div>
     </main>
